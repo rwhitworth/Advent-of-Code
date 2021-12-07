21296c31058e97fa5083d6d53c0988828712973d
Ryan Whitworth <me@ryanwhitworth.com>
2021-12-07 15:33:29 -0500

    Create 06-01.rb

---
+++ 2021/06-01.rb
@@ -0,0 +1,18 @@
+a = IO.read('input/6').split(',').collect {|x| x.to_i };
+q = 9.times.collect {|x| 0 }
+a.each do |x|
+  q[x] = q[x] + 1
+end;
+80.times do |count|
+  # 9 total buckets, but bucket 0 is not really processed, it just gets trashed
+  old_zero = q[0]
+
+  8.times do |counter2|
+    q[counter2] = q[counter2 + 1]
+  end
+
+  q[8] = old_zero
+  q[6] = q[6] + old_zero
+end;
+
+q.sum
