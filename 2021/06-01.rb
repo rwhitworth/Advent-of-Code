9c9a2c213253280dd9d6bf72dd5b020e2871247b
Ryan Whitworth <me@ryanwhitworth.com>
2021-12-07 15:38:00 -0500

    Update 06-01.rb

--- 2021/06-01.rb
+++ 2021/06-01.rb
@@ -16,3 +16,35 @@ end;
 end;
 
 q.sum
+
+
+
+# first solution is below, but it has a major drawback in regard to runtime speed
+# the above solution uses a handful of buckets and has many fewer memory allocations
+
+#def age_fish(school)
+#  growth = 0
+#  res = school.collect do |fish|
+#    if fish == 0
+#      growth = growth + 1
+#      6
+#    else
+#      fish - 1
+#    end
+#  end
+#  [res, growth]
+#end
+
+## example input
+## a = '3,4,3,1,2'.split(',').collect {|x| x.to_i };
+#a = IO.read('input/6').split(',').collect {|x| x.to_i };
+
+#80.times do |count|
+#  res, growth = age_fish(a);
+#  growth.times do |counter2|
+#    res << 8
+#  end
+#  a = res
+#end
+
+#puts a.count
